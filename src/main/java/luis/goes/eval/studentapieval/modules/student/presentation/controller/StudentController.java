43b6c0ccb52a145db26150d3f3857a56caa2640a
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-07-09 14:44:44 -0300

    Feat: Criação do `StudentController.

---
+++ src/main/java/luis/goes/eval/studentapieval/modules/student/presentation/controller/StudentController.java
@@ -0,0 +1,51 @@
+package luis.goes.eval.studentapieval.modules.student.presentation.controller;
+
+import luis.goes.eval.studentapieval.modules.student.application.useCase.StudentUseCase;
+import luis.goes.eval.studentapieval.modules.student.presentation.dto.StudentRequestDto;
+import luis.goes.eval.studentapieval.modules.student.presentation.dto.StudentResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/api/v1/student")
+public class StudentController {
+    private final StudentUseCase studentUseCase;
+
+    public StudentController(StudentUseCase studentUseCase) {
+        this.studentUseCase = studentUseCase;
+    }
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<StudentResponseDto> getById(@PathVariable UUID id) {
+        return ResponseEntity.ok().body(studentUseCase.getGetterMethods().getById(id));
+    }
+
+    @GetMapping(value = "/name/{name}")
+    public ResponseEntity<StudentResponseDto> getByName(@PathVariable String name) {
+        return ResponseEntity.ok().body(studentUseCase.getGetterMethods().getByName(name));
+    }
+
+    @GetMapping(value = "/cpf{cpf}")
+    public ResponseEntity<StudentResponseDto> getByCpf(@PathVariable String cpf) {
+        return ResponseEntity.ok().body(studentUseCase.getGetterMethods().getByCpf(cpf));
+    }
+
+    @GetMapping(value = "{email}")
+    public ResponseEntity<StudentResponseDto> getByEmail(@PathVariable String email) {
+        return ResponseEntity.ok().body(studentUseCase.getGetterMethods().getByEmail(email));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StudentResponseDto>> getAll() {
+        return ResponseEntity.ok().body(studentUseCase.getGetterMethods().getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<StudentResponseDto> create(@RequestBody StudentRequestDto dto) {
+        return ResponseEntity.ok().body(studentUseCase.getCreate().create(dto));
+    }
+
+}
