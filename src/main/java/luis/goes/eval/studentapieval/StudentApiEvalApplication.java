6492455aa946301d87ddcac3ad2d21f0422b0c95
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-07-09 11:04:56 -0300

    Feat: Início do projeto

---
+++ src/main/java/luis/goes/eval/studentapieval/StudentApiEvalApplication.java
@@ -0,0 +1,13 @@
+package luis.goes.eval.studentapieval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApiEvalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentApiEvalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/luis/goes/eval/studentapieval/StudentApiEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package luis.goes.eval.studentapieval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApiEvalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
