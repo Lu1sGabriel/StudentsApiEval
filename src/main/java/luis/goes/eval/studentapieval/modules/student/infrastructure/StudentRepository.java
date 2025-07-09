130f2a411a52d2c68dece76e7d867ce4b2b704db
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-07-09 12:06:33 -0300

    Feat: Criação do `StudentRepository` já com alguns métodos auxiliares

---
+++ src/main/java/luis/goes/eval/studentapieval/modules/student/infrastructure/StudentRepository.java
@@ -0,0 +1,23 @@
+package luis.goes.eval.studentapieval.modules.student.infrastructure;
+
+import luis.goes.eval.studentapieval.modules.student.domain.StudentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface StudentRepository extends JpaRepository<StudentEntity, UUID> {
+    Optional<StudentEntity> findByStudentInfo_Name_Name(String name);
+
+    Optional<StudentEntity> findByStudentInfo_Cpf_Cpf(String cpf);
+
+    Optional<StudentEntity> findByStudentInfo_Email_Email(String email);
+
+    @Query("SELECT s.studentInfo.email.email FROM StudentEntity s")
+    List<String> findAllEmail();
+
+}
