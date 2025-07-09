16d3a89a9fd92bedcdf09d4b336c098d32d0aa74
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-07-09 11:55:54 -0300

    Feat: Criação do `GlobalExceptionHandler` para tratar os erros do back-end e retorná-los de forma amigável para o usuário

---
+++ src/main/java/luis/goes/eval/studentapieval/core/exception/HttpException.java
@@ -0,0 +1,33 @@
+package luis.goes.eval.studentapieval.core.exception;
+
+
+import luis.goes.eval.studentapieval.core.shared.utils.StatusCode;
+
+public class HttpException extends RuntimeException {
+    private final int statusCode;
+
+    public HttpException(String message, StatusCode statusCode) {
+        super(message);
+        this.statusCode = statusCode.getCode();
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public static HttpException notFound(String message) {
+        return new HttpException(message, StatusCode.NOT_FOUND);
+    }
+
+    public static HttpException badRequest(String message) {
+        return new HttpException(message, StatusCode.BAD_REQUEST);
+    }
+
+    public static HttpException unauthorized(String message) {
+        return new HttpException(message, StatusCode.UNAUTHORIZED);
+    }
+
+    public static HttpException forbidden(String message) {
+        return new HttpException(message, StatusCode.FORBIDDEN);
+    }
+}
