a673913b10e410d7d594d5912dba624f289718e9
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-07-09 14:41:05 -0300

    Refacotr: Agora o método getAll já filtra os estudantes que não estão desativados.

--- src/main/java/luis/goes/eval/studentapieval/modules/student/infrastructure/StudentRepository.java
+++ src/main/java/luis/goes/eval/studentapieval/modules/student/infrastructure/StudentRepository.java
@@ -1,6 +1,7 @@
 package luis.goes.eval.studentapieval.modules.student.infrastructure;
 
 import luis.goes.eval.studentapieval.modules.student.domain.StudentEntity;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -17,7 +18,9 @@ public interface StudentRepository extends JpaRepository<StudentEntity, UUID> {
 
     Optional<StudentEntity> findByStudentInfo_Email_Email(String email);
 
+    List<StudentEntity> findByStudentDateInfo_DeletedAtIsNull(Sort sort);
+
     @Query("SELECT s.studentInfo.email.email FROM StudentEntity s")
-    List<String> findAllEmail();
+    List<String> findAllEmails();
 
 }
