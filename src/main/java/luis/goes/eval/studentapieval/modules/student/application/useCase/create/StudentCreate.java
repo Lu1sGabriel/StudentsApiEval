b60975c68fe86e002413a8805640361a9b1ded59
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-07-11 11:57:07 -0300

    Refactor: Adição da validação para dados que já existem e não pode ser duplicados. Já fazia isso, porém em uma camada mais baixa.

--- src/main/java/luis/goes/eval/studentapieval/modules/student/application/useCase/create/StudentCreate.java
+++ src/main/java/luis/goes/eval/studentapieval/modules/student/application/useCase/create/StudentCreate.java
@@ -1,5 +1,6 @@
 package luis.goes.eval.studentapieval.modules.student.application.useCase.create;
 
+import luis.goes.eval.studentapieval.core.exception.HttpException;
 import luis.goes.eval.studentapieval.core.shared.utils.EmailGenerator;
 import luis.goes.eval.studentapieval.modules.student.domain.StudentEntity;
 import luis.goes.eval.studentapieval.modules.student.domain.StudentInfo;
@@ -24,19 +25,25 @@ public class StudentCreate implements IStudentCreate {
 
     @Override
     public StudentResponseDto create(final StudentRequestDto dto) {
-        String email = generateStudentEmail(dto.name());
-        StudentInfo studentInfo = createStudentInfo(dto.name(), dto.cpf(), email);
+        checkIfFieldsAlreadyExists(dto);
+        String email = generateStudentEmail(dto);
+        StudentInfo studentInfo = createStudentInfo(dto, email);
         StudentEntity student = new StudentEntity(studentInfo, dto.grade());
         return mapper.toDto(repository.save(student));
     }
 
-    private StudentInfo createStudentInfo(String name, String cpf, String email) {
-        return new StudentInfo(name, cpf, email);
+    private StudentInfo createStudentInfo(StudentRequestDto dto, String email) {
+        return new StudentInfo(dto.name(), dto.cpf(), email);
     }
 
-    private String generateStudentEmail(String fullName) {
+    private String generateStudentEmail(StudentRequestDto dto) {
         Set<String> emailSet = new HashSet<>(repository.findAllEmails());
-        return EmailGenerator.generateFromName(fullName, emailSet);
+        return EmailGenerator.generateFromName(dto.name(), emailSet);
+    }
+
+    private void checkIfFieldsAlreadyExists(StudentRequestDto dto) {
+        if (repository.findByStudentInfo_Name_Name(dto.name()).isPresent()) throw HttpException.conflict("Already has a student with this name.");
+        if (repository.findByStudentInfo_Cpf_Cpf(dto.cpf()).isPresent()) throw HttpException.conflict("Already has a student with this CPF.");
     }
 
 }
